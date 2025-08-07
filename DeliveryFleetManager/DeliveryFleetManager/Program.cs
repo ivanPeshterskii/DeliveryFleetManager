fb9bda82d28376a969110f269e624006efe541a1
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-08-07 03:19:14 +0300

    Upload project

---
+++ DeliveryFleetManager/DeliveryFleetManager/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using DeliveryFleetManager.VanInformation;
+
+namespace DeliveryFleetManager
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Van> vans = new Dictionary<string, Van>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split('|');
+                string name = input[0];
+                int mileage = int.Parse(input[1]);
+                int fuel = int.Parse(input[2]);
+
+                vans[name] = new Van(name, mileage, fuel);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "Shutdown")
+            {
+                string[] parts = command.Split(" : ");
+                string action = parts[0];
+                string vanName = parts[1];
+
+                if (!vans.ContainsKey(vanName))
+                    continue;
+
+                Van van = vans[vanName];
+
+                switch (action)
+                {
+                    case "Deliver":
+                        int distance = int.Parse(parts[2]);
+                        int fuel = int.Parse(parts[3]);
+
+                        
+                        string deliveryResult = van.Deliver(distance, fuel);
+                        Console.WriteLine(deliveryResult);
+
+                        if (van.Mileage >= 150000)
+                        {
+                            Console.WriteLine($"Van {van.Name} is decommissioned");
+                            vans.Remove(vanName);
+                        }
+
+                        break;
+
+                    case "Refuel":
+                        int refuelAmount = int.Parse(parts[2]);
+                        string refuelResult = van.Refuel(refuelAmount);
+                        Console.WriteLine(refuelResult);
+                        break;
+
+                    case "Service":
+                        int kilometers = int.Parse(parts[2]);
+                        string serviceResult = van.Service(kilometers);
+                        if (!string.IsNullOrEmpty(serviceResult))
+                        {
+                            Console.WriteLine(serviceResult);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var van in vans.Values)
+            {
+                Console.WriteLine(van);
+            }
+        }
+    }
+}
---
+++ DeliveryFleetManager/DeliveryFleetManager/VanInformation/Van.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace DeliveryFleetManager.VanInformation
+{
+    public class Van
+    {
+        public string Name { get; set; }
+        public int Mileage { get; private set; }
+        public int Fuel { get; private set; }
+
+        private const int MaxFuel = 100;
+        private const int MinMileage = 5000;
+        private const int MaxMileage = 150000;
+
+        public Van(string name, int mileage, int fuel)
+        {
+            Name = name;
+            Mileage = mileage;
+            Fuel = fuel;
+        }
+
+        public string Deliver(int distance, int fuelNeeded)
+        {
+            
+
+            if (Fuel < fuelNeeded)
+            {
+                return "Insufficient fuel for the delivery";
+            }
+
+            Mileage += distance;
+            Fuel -= fuelNeeded;
+
+            string message = $"Delivery van {Name} drove {distance} km using {fuelNeeded} liters";
+
+            if (Mileage >= MaxMileage)
+            {
+                
+                message += $"\nVan {Name} is decommissioned";
+            }
+
+            return message;
+        }
+
+        public string Refuel(int fuelAmount)
+        {
+            int fuelBefore = Fuel;
+            int fuelAdded = Math.Min(fuelAmount, MaxFuel - Fuel);
+            Fuel += fuelAdded;
+
+            return $"Van {Name} refueled with {fuelAdded} liters";
+        }
+
+        public string Service(int km)
+        {
+            int oldMileage = Mileage;
+            Mileage -= km;
+
+            if (Mileage < MinMileage)
+            {
+                Mileage = MinMileage;
+                return "";
+            }
+
+            int reducedBy = oldMileage - Mileage;
+            return $"Van {Name} mileage decreased by {reducedBy} km";
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} -> Mileage: {Mileage} km, Fuel: {Fuel} lt.";
+        }
+    }
+}
